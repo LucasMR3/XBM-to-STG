d91a6bd5c9ed88b052cc852cdf81c7ef6c57a017
LucasMR3 <lucas_mr@live.com>
2021-06-07 12:38:05 -0300

    Compiler working with simple examples

--- src/main/java/stg/Compiler.java
+++ src/main/java/stg/Compiler.java
@@ -2,6 +2,9 @@ package stg;
 
 import model.XBMCode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Compiler {
     XBMCode x;
 
@@ -10,21 +13,25 @@ public class Compiler {
     }
 
     public void Compile() {
+        List<String> write = new ArrayList<>();
 //        System.out.println(compiler);
 
         System.out.print("\n.inputs ");
         for (int i = 0; i < x.inputs().size(); i++) {
             System.out.print(x.inputs().get(i).getName() + " ");
+            write.add(x.inputs().get(i).getName() + " ");
         }
 
         System.out.print("\n.outputs ");
         for (int i = 0; i < x.outputs().size(); i++) {
             System.out.print(x.outputs().get(i).getName() + " ");
+            write.add(x.outputs().get(i).getName() + " ");
         }
 
-        System.out.print("\n.graph");
+        System.out.print("\n.graph\n");
+        write.add("\n.graph\n");
 
-        System.out.print("\n\nSTEPS\n");
+//        System.out.print("\n\nSTEPS\n");
 
         int places = -1;
         String last = null;
@@ -34,27 +41,37 @@ public class Compiler {
 
             for (int j = 0; j < x.steps().get(i).getInXBMStep().size(); j++) {
                 places++;
-                if (last != null)
-                    System.out.println(last);
-
-                System.out.print("p" + places + " ");
-                System.out.println(x.steps().get(i).getInXBMStep().get(j) + " ");
-                last = String.valueOf(x.steps().get(i).getInXBMStep().get(j));
-
+                if (last != null) {
+                    System.out.println(last + " p" + places + " ");
+                    System.out.println("p" + places + " " + x.steps().get(i).getInXBMStep().get(j));
+                    last = String.valueOf(x.steps().get(i).getInXBMStep().get(j));
+                } else {
+                    System.out.print("p" + places + " ");
+                    System.out.println(x.steps().get(i).getInXBMStep().get(j) + " ");
+                    last = String.valueOf(x.steps().get(i).getInXBMStep().get(j));
+                }
+            }
                 for (int h = 0; h < x.steps().get(h).getOutXBMStep().size(); h++) {
                     places++;
                     System.out.println(last + " p" + places + " ");
-//                    System.out.println("o/");
-                    System.out.println(x.steps().get(i).getOutXBMStep().get(h) + " ");
+                    System.out.println("p" + places + " " + x.steps().get(i).getOutXBMStep().get(h));
                     last = String.valueOf(x.steps().get(i).getOutXBMStep().get(h));
+
+                    if (x.steps().size() == i + 1){
+                        System.out.println(last + " p" + 0 + " ");
+                    }
                 }
 //                System.out.println(last);
-            }
-
             System.out.println();
         }
 
+        System.out.println(".marking {p0} ");
+        System.out.println(".end");
+
         System.out.println();
+
+
+        WriteFile.write("file", write);
 
     }
 
---
+++ src/main/java/stg/WriteFile.java
@@ -0,0 +1,38 @@
+package stg;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class WriteFile {
+
+    public static void write(String fileName, List<String> content) {
+
+        try {
+            File file = new File("out/" + fileName);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            bw.write(evaluateContent(content));
+            bw.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String evaluateContent(List<String> content) {
+        StringBuilder a = new StringBuilder();
+        for (String str : content) {
+            a.append('\n').append(str);
+        }
+        return a.toString();
+    }
+}
